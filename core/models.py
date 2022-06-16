cd42f4d87cf561b05d7ffad7f432737baeefb3e4
Gabriel Magno <gabrielmzero0@gmail.com>
2022-06-16 20:38:31 -0300

    Função choices na model

--- core/models.py
+++ core/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class Manga(models.Model):
     STATUS_CHOICES = (
-        ('Em lançamento'),
-        ('Finalizado'),
-        ('Em hiato')
+        ('L', 'Lançamento'),
+        ('F', 'Finalizado'),
+        ('I', 'Indefinido'),
     )
-    nome = models.CharField('Nome', max_length=100)
-    capitulos = models.IntegerField('Capitulos')
-    genero = models.CharField('Genero', max_length=100)
-    status = models.CharField('Status', max_length=1, choices=STATUS_CHOICES)
+    nome = models.CharField(name='Nome', max_length=100)
+    capitulos = models.IntegerField(name='Capitulos')
+    genero = models.CharField(name='Genero', max_length=100)
+    status = models.CharField(name='Status', max_length=1, choices=STATUS_CHOICES)
 
     def __str__(self):
         return self.nome
