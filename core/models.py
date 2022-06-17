ae96b65123c5c738ca7bca09837e3ba73ca93e31
Gabriel Magno <gabrielmzero0@gmail.com>
2022-06-16 21:15:45 -0300

    Erros corrigidos

--- core/models.py
+++ core/models.py
@@ -2,11 +2,6 @@ from django.db import models
 
 
 class Manga(models.Model):
-    GENERO_CHOICES = (
-        ('SE', 'Seinen'),
-        ('SH', 'Shounen')
-        ('OU', 'Outro')
-    )
     STATUS_CHOICES = (
         ('L', 'Lançamento'),
         ('F', 'Finalizado'),
@@ -14,7 +9,7 @@ class Manga(models.Model):
     )
     nome = models.CharField(name='nome', max_length=100)
     capitulos = models.IntegerField(name='capitulos')
-    genero = models.CharField(name='genero', max_length=1, choices=GENERO_CHOICES)
+    genero = models.CharField(name='genero', max_length=10)
     status = models.CharField(name='status', max_length=1, choices=STATUS_CHOICES)
 
     def __str__(self):
