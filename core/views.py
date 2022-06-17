90f844835bad30942a3fc4d2dffa095d865bdae7
Gabriel Magno <gabrielmzero0@gmail.com>
2022-06-16 22:28:24 -0300

    Adicionando dados do db para o template

--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Manga
 
 def index(request):
     return render(request, 'index.html')
 
 def mangas(request):
-    return render(request, 'mangas.html')
+    mang = Manga.objects.all()
+    context = {
+        'mang': mang
+    }
+    return render(request, 'mangas.html', context)
