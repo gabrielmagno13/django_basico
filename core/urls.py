6a4e3c49cce57112e3323a70c988045a39b18446
Gabriel Magno <gabrielmzero0@gmail.com>
2022-06-16 21:43:32 -0300

    Criado um novo template

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, mangas
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    path('mangas/', mangas, name='mangas')
 ]
